96ee91980e4f139271bd84aca80358b5f4709ae1
sloane <132950675+sloaneeliza@users.noreply.github.com>
2025-02-08 19:14:33 -0600

    Update ProjCard.js

--- src/components/ProjCard.js
+++ src/components/ProjCard.js
@@ -5,6 +5,7 @@ export const ProjCard = ({title, description, imgUrl, link}) => {
     return(
         <Col sm={6} md={4}>
             <div className ="proj-imgbx">
+              <img src ={imgUrl} />
                 <div className ="proj-txtx">
                     <h4>{title}</h4>
                     <span>{description}</span>
