f95d2de8ca5f27f302bf72c5d38d088d547c5016
sloane <132950675+sloaneeliza@users.noreply.github.com>
2025-02-08 19:14:13 -0600

    Update Proj.js

--- src/components/Proj.js
+++ src/components/Proj.js
@@ -12,6 +12,7 @@ export const Proj = () => {
     {
       title: "Eclipse",
       description: "A mindfulness app dedicated to students. It includes pomodoro timers, ambient noises, and guided timed meditation.",
+      imgUrl: Eclipse,
       link: "https://github.com/sloaneeliza/Eclipse"
     },
     {
